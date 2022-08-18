7c2c6992cf7a81f71e4386100d1f1fe2c0ca70d9
victor <vcamara@popos>
2022-08-18 20:10:17 +0200

    Lifecycle of the application is done.

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       monsterHealth: 100,
-      monsterDanger: false,
       playerHealth: 100,
-      playerDanger: false,
       currentTurn: 0,
       winner: null,
+      combatLog: [],
     };
   },
   watch: {
@@ -48,31 +47,49 @@ const app = Vue.createApp({
       this.playerHealth = 100;
       this.winner = null;
       this.currentTurn = 0;
+      this.combatLog = [];
+    },
+
+    addLogRecord(who, type, value) {
+      this.combatLog.unshift({
+        actionWho: who,
+        actionType: type,
+        actionValue: value,
+      });
     },
 
     surrender() {
       this.winner = "Monster";
       this.playerHealth = 0;
     },
+
     // Player attacks the monster and will recieve the monster's attack as well.
     playerAttack() {
       const playerDamage = this.generateStat(12, 18);
       this.currentTurn++;
       this.monsterHealth -= playerDamage;
       this.monsterAttack();
+      this.addLogRecord("player", "attack", playerDamage);
     },
+
     monsterAttack() {
       const monsterDamage = this.generateStat(10, 15);
       this.playerHealth -= monsterDamage;
+      this.addLogRecord("monster", "attack", monsterDamage);
     },
+
     playerSuperAttack() {
       const monsterDamage = this.generateStat(10, 15);
       const playerDamage = this.generateStat(20, 30);
       this.currentTurn++;
       this.playerHealth -= monsterDamage;
       this.monsterHealth -= playerDamage;
+      this.addLogRecord("player", "super-attack", playerDamage);
+      this.addLogRecord("monster", "attack", monsterDamage);
     },
+
     // As the player's health is linked up with the width of the element, we don't want to exceed 100%.
+    // The monster won't attack if the player is healing.
     playerHeal() {
       this.currentTurn++;
 
@@ -82,7 +99,9 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healthRecover;
       }
+      this.addLogRecord("player", "heal", healthRecover);
     },
+
     // A stat will be used so the player can attack and heal within the same range.
     generateStat(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
